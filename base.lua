a8dfdf83304881ba1907478cdf153bc8b761329d
VIP-GOLD <40060266+VIP-GOLD@users.noreply.github.com>
2018-06-08 15:40:24 +0300

    Update base.lua

--- base.lua
+++ base.lua
@@ -1,14 +1,14 @@
 serpent = (loadfile "serpent.lua")()
 tdcli = dofile('tdcli.lua')
 redis = (loadfile "redis.lua")()
-gold_id = "GOLD-ID"
+VIP-CONTROL_id = "VIP-CONTROL-ID"
 
 function vardump(value)
   return serpent.block(value,{comment=false})
 end
 
 function reload()
-  gold = dofile("GOLD.lua")
+  VIP-CONTROL = dofile("VIP-CONTROL.lua")
 end
 
 function dl_cb (arg, data)
@@ -17,11 +17,11 @@ end
 reload()
 
 function tdcli_update_callback(data)
-  gold.update(data, gold_id)
-  if data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/reload" and data.message_.sender_user_id_ == tonumber(redis:get("gold:" .. gold_id ..":fullsudo")) then
+  VIP-CONTROL.update(data, VIP-CONTROL_id)
+  if data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/reload" and data.message_.sender_user_id_ == tonumber(redis:get("VIP-CONTROL:" .. VIP-CONTROL_id ..":fullsudo")) then
     reload()
     tdcli.sendMessage(data.message_.chat_id_, 0, 1, "- تم اعادة التحميل ✅", 1, "md")
-  elseif data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/gitpull" and data.message_.sender_user_id_ == tonumber(redis:get("gold:" .. gold_id ..":fullsudo")) then
+  elseif data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/gitpull" and data.message_.sender_user_id_ == tonumber(redis:get("VIP-CONTROL:" .. VIP-CONTROL_id ..":fullsudo")) then
     io.popen("git fetch --all && git reset --hard origin/master && git pull origin master"):read("*all")
     reload()
     tdcli.sendMessage(data.message_.chat_id_, 0, 1, "- تم تحديث البوت واعادة التحميل ✅", 1, "md")
