9b53dc0874794ec612352bed6269c32b5e479867
VIP-GOLD <40060266+VIP-GOLD@users.noreply.github.com>
2018-06-09 02:06:02 +0300

    Update manual-creator.lua

--- manual-creator.lua
+++ manual-creator.lua
@@ -8,5 +8,4 @@ io.open("CONTROL-"..last..".lua",'w'):write(text):close()
 io.open("CONTROL-"..last..".sh",'w'):write("while true; do\n./telegram-cli-1222 -p CONTROL-"..last.." -s CONTROL-"..last..".lua\ndone"):close()
 io.popen("chmod 777 CONTROL-"..last..".sh")
 redis:set('gold:'..last..':fullsudo',sudo)
-print("✔️ THANK YOU FOR INSTALL CONTROL BOT ✔️
-♛ Source Created By @amody7 On Telegram ♛\nDone!\nNew bot Control Created...\nID : "..last.."\nFull Sudo : "..sudo.."\nRun : ./CONTROL-"..last..".sh")
+print("✔️ THANK YOU FOR INSTALL CONTROL BOT ✔️\n♛ Source Created By @amody7 On Telegram ♛\nDone!\nNew bot Control Created...\nID : "..last.."\nFull Sudo : "..sudo.."\nRun : ./CONTROL-"..last..".sh")
