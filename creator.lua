9afb3a9d914f127f58affe6690703781d7dbb91d
VIP-GOLD <40060266+VIP-GOLD@users.noreply.github.com>
2018-06-08 15:42:52 +0300

    Update creator.lua

--- creator.lua
+++ creator.lua
@@ -1,22 +1,22 @@
 redis = (loadfile "redis.lua")()
-function getgoldid()
+function getVIP-CONTROLid()
     local i, t, popen = 0, {}, io.popen
     local pfile = popen('ls')
 	local last = 0
     for filename in pfile:lines() do
-        if filename:match('gold%-(%d+)%.lua') and tonumber(filename:match('gold%-(%d+)%.lua')) >= last then
-			last = tonumber(filename:match('gold%-(%d+)%.lua')) + 1
+        if filename:match('VIP-CONTROL%-(%d+)%.lua') and tonumber(filename:match('gold%-(%d+)%.lua')) >= last then
+			last = tonumber(filename:match('VIP-CONTROL%-(%d+)%.lua')) + 1
 			end		
     end
     return last
 end
-local last = getgoldid()
-io.write("Auto Detected gold ID : "..last)
+local last = getVIP-CONTROLid()
+io.write("Auto Detected VIP-CONTROL ID : "..last)
 io.write("\nEnter or set Sudo ID : ")
 local sudo=io.read()
-local text,ok = io.open("base.lua",'r'):read('*a'):gsub("gold%-ID",last)
-io.open("gold-"..last..".lua",'w'):write(text):close()
-io.open("gold-"..last..".sh",'w'):write("while true; do\n$(dirname $0)/telegram-cli-1222 -p gold-"..last.." -s gold-"..last..".lua\ndone"):close()
-io.popen("chmod 777 gold-"..last..".sh")
-redis:set('gold:'..last..':fullsudo',sudo)
-print("Done!\nNew bot Created...\nID : "..last.."\nFull Sudo : "..sudo.."\nRun : ./gold-"..last..".sh")
+local text,ok = io.open("base.lua",'r'):read('*a'):gsub("VIP-CONTROL%-ID",last)
+io.open("VIP-CONTROL-"..last..".lua",'w'):write(text):close()
+io.open("gold-"..last..".sh",'w'):write("while true; do\n$(dirname $0)/telegram-cli-1222 -p VIP-CONTROL-"..last.." -s VIP-CONTROL-"..last..".lua\ndone"):close()
+io.popen("chmod 777 VIP-CONTROL-"..last..".sh")
+redis:set('VIP-CONTROL:'..last..':fullsudo',sudo)
+print("Done!\nNew bot Control Created...\nID : "..last.."\nFull Sudo : "..sudo.."\nRun : ./VIP-CONTROL-"..last..".sh")
