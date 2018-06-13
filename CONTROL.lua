cc433c31bd8922d64cf51d4be591b1aec1b8b48c
VIP-GOLD <40060266+VIP-GOLD@users.noreply.github.com>
2018-06-14 01:26:50 +0300

    Update CONTROL.lua

--- CONTROL.lua
+++ CONTROL.lua
@@ -95,10 +95,10 @@ function our_id(extra, result)
 end
 local process_links
 function process_links(text)
-  if text:match("https://telegram.me/joinchat/%S+") or text:match("https://telegram.dog/joinchat/%S+") or text:match("https://t.dog/joinchat/%S+") then
+  if text:match("https://t.me/joinchat/%S+") or text:match("https://t.me/joinchat/%S+") or text:match("https://t.me/joinchat/joinchat/%S+") then
     text = text:gsub("t.me", "telegram.me")
     local matches = {
-      text:match("(https://telegram.me/joinchat/%S+)")
+      text:match("(https://t.me/joinchat/%S+)")
     }
     for i, v in pairs(matches) do
       tdcli_function({
